cba4cbb9c8dddbfe5f490ff52658e76b205551e6
shleewhite <lwhite@sportsbasement.com>
2015-01-28 11:36:14 -0800

    made fractal, squares still not in right place

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
 
 
 public void setup()
@@ -9,7 +25,7 @@ public void draw()
 {
 	background(0);
 	stroke(100);
-	strokeWeight(0.5);
+	strokeWeight(0.5f);
 	myFractal(50, 50, 150);
 	myFractal(150, 150, 150);
 	myFractal(250, 250, 150);
@@ -36,4 +52,13 @@ public void myFractal(int x, int y, int siz)
 		rect(x - siz/2, y + siz, siz/2, siz/2);
 		rect(x - siz/2, y - siz/2, siz/2, siz/2);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
